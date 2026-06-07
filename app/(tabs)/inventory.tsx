d5e5c8cd36d4f9bc4c7c3e949104970cb402ef32
Szymon <sztrofimiec@gmail.com>
2026-06-07 19:04:56 +0200

    item deletions

--- app/(tabs)/inventory.tsx
+++ app/(tabs)/inventory.tsx
@@ -156,7 +156,6 @@ export default function InventoryScreen() {
                         onSearchChange={() => {}}
                         onFilterPress={() => {}}
                         onCreateItem={handleCreateItem}
-                        onDeletePress={() => {}}
                     />
 
                     {!isLoading &&
